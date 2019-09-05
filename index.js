f1abfd217bd33639f82a39f9375488d36d0597d1
Tobie Tsuzuki <tobie.tsuzuki@gmail.com>
2019-09-05 01:24:57 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,36 @@
+var katzDeliLine = []
+function takeANumber(katzDeliLine, name) {
+	katzDeliLine.push(name);
+	return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line.";
+};
+var cusNumber = 1
+function takeANumber2() {
+  cusNumber += 1
+  console.log("Welcome, your customer number is: " + cusNumber);
+}
+
+takeANumber2();
+takeANumber2();
+
+function nowServing(katzDeliLine){
+		if(katzDeliLine.length === 0){
+		return "There is nobody waiting to be served!";
+	} else {
+    var name = katzDeliLine[0];
+		katzDeliLine.shift();
+    return "Currently serving " + name + ".";
+  }
+};
+
+function currentLine(katzDeliLine) {
+  var line = [];
+    if (katzDeliLine.length === 0) {
+      return "The line is currently empty."
+    } else {
+      for(var i = 0; i < katzDeliLine.length; i++) {
+        line.push(" "+ (i + 1) + ". " + katzDeliLine[i]);
+        }
+      return "The line is currently:" + line;
+    }
+};
+  
